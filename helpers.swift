f64a3f4a95a52ee56baae3d26a24d284f194168a
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-13 09:23:53 +0000

    Matches helpers with 2021

--- helpers.swift
+++ helpers.swift
@@ -117,20 +117,71 @@ struct Regex {
     }
 }
 
-func getCombinations(input: [Int], count: Int) -> [[Int]] {
-    if count == 0 { return [[]] }
+extension Array where Element: Equatable {
+    func combinations(count: Int) -> [[Element]] {
+        if count == 0 { return [[]] }
 
-    if count == 1 { return input.map({ [$0] }) }
+        if count == 1 { return self.map({ [$0] }) }
 
-    let previousCombinations = getCombinations(input: input, count: count - 1)
-    
-    var combinations: [[Int]] = []
+        let previousCombinations = combinations(count: count - 1)
 
-    for i in (0..<input.count) {
-        for j in (0..<previousCombinations.count) where !previousCombinations[j].contains(input[i]) {
-            combinations.append(previousCombinations[j] + [input[i]])
+        var combinations: [[Element]] = []
+
+        for i in (0..<count) {
+            for j in (0..<previousCombinations.count) where !previousCombinations[j].contains(self[i]) {
+                combinations.append(previousCombinations[j] + [self[i]])
+            }
         }
+
+        return combinations
+    }
+}
+
+// Coordinate
+struct Coordinate: Hashable {
+    let x: Int
+    let y: Int
+
+    typealias FoldLine = (axis: Axis, location: Int)
+
+    enum Axis: String {
+        case y
+        case x
     }
 
-    return combinations
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
+    }
+
+    var adjacents: [Coordinate] {
+        var adjacents: [Coordinate] = []
+        for x in x-1...x+1 {
+            for y in y-1...y+1  {
+                if x == self.x && y == self.y { continue }
+                adjacents.append(Coordinate(x, y))
+            }
+        }
+        return adjacents
+    }
+
+    func getAdjacents(in grid: [[Any]]) -> [Coordinate] {
+        var adjacents: [Coordinate] = []
+        for x in x-1...x+1 where x >= 0 && x < grid[0].count {
+            for y in y-1...y+1 where y >= 0 && y < grid.count {
+                if x == self.x && y == self.y { continue }
+                adjacents.append(Coordinate(x, y))
+            }
+        }
+
+        return adjacents
+    }
+
+    func translate(along foldLine: FoldLine) -> Coordinate {
+        let currentValue = foldLine.axis == .y ? y : x
+        let difference = (currentValue - foldLine.location) * 2
+        let newValue = currentValue - difference
+
+        return Coordinate(foldLine.axis == .y ? x : newValue, foldLine.axis == .y ? newValue : y)
+    }
 }
